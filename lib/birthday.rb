eac173e598748d626f8ac905ba2e95f8d99f217c
Jimmy Millard <millardjimmy@gmail.com>
2020-05-10 16:25:27 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -18,6 +18,7 @@ end
   birthday_kids.each do |name, age|
     if age < 13
     puts "Happy Birthday #{name}! You are now #{age} years old!"
+  else puts "You are too old for this"
   end
   end
 end
